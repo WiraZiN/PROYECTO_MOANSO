640883d563389f8e5d5a1342f353c5bef3f54831
WiraZiN <wirazin.de26@gmail.com>
2023-10-24 23:44:32 -0500

    He añado el Mantenedor Prenda

--- PROYECTO_MOANSO/Program.cs
+++ PROYECTO_MOANSO/Program.cs
@@ -16,7 +16,7 @@ namespace PROYECTO_MOANSO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MantenedorPrenda());
         }
     }
 }
