d6e14ec60b8cdec25a1a19b47cefd593619b80ad
Irvin Yoel <irvinyoel.cerna@gmail.com>
2023-10-25 00:15:10 -0500

    CREACION INTERFAZ PRODUCTO

---
+++ PROYECTO_MOANSO/PRODUCTO.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace PROYECTO_MOANSO
+{
+    partial class PRODUCTO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInhabilitar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnNuevoMarca = new System.Windows.Forms.Button();
+            this.btnNuevoMaterial = new System.Windows.Forms.Button();
+            this.btnNuevoTalla = new System.Windows.Forms.Button();
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
+            this.cmbMaterial = new System.Windows.Forms.ComboBox();
+            this.cmbTalla = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodProducto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInhabilitar
+            // 
+            this.btnInhabilitar.Location = new System.Drawing.Point(42, 370);
+            this.btnInhabilitar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnInhabilitar.Name = "btnInhabilitar";
+            this.btnInhabilitar.Size = new System.Drawing.Size(98, 32);
+            this.btnInhabilitar.TabIndex = 45;
+            this.btnInhabilitar.Text = "INHABILITAR";
+            this.btnInhabilitar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(42, 306);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(98, 32);
+            this.btnAgregar.TabIndex = 44;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevoMarca
+            // 
+            this.btnNuevoMarca.Location = new System.Drawing.Point(370, 175);
+            this.btnNuevoMarca.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNuevoMarca.Name = "btnNuevoMarca";
+            this.btnNuevoMarca.Size = new System.Drawing.Size(56, 19);
+            this.btnNuevoMarca.TabIndex = 43;
+            this.btnNuevoMarca.Text = "NUEVO";
+            this.btnNuevoMarca.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevoMaterial
+            // 
+            this.btnNuevoMaterial.Location = new System.Drawing.Point(370, 140);
+            this.btnNuevoMaterial.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNuevoMaterial.Name = "btnNuevoMaterial";
+            this.btnNuevoMaterial.Size = new System.Drawing.Size(56, 19);
+            this.btnNuevoMaterial.TabIndex = 42;
+            this.btnNuevoMaterial.Text = "NUEVO";
+            this.btnNuevoMaterial.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevoTalla
+            // 
+            this.btnNuevoTalla.Location = new System.Drawing.Point(370, 108);
+            this.btnNuevoTalla.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNuevoTalla.Name = "btnNuevoTalla";
+            this.btnNuevoTalla.Size = new System.Drawing.Size(56, 19);
+            this.btnNuevoTalla.TabIndex = 41;
+            this.btnNuevoTalla.Text = "NUEVO";
+            this.btnNuevoTalla.UseVisualStyleBackColor = true;
+            // 
+            // cmbMarca
+            // 
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Location = new System.Drawing.Point(204, 175);
+            this.cmbMarca.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(151, 21);
+            this.cmbMarca.TabIndex = 40;
+            // 
+            // cmbMaterial
+            // 
+            this.cmbMaterial.FormattingEnabled = true;
+            this.cmbMaterial.Location = new System.Drawing.Point(204, 140);
+            this.cmbMaterial.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbMaterial.Name = "cmbMaterial";
+            this.cmbMaterial.Size = new System.Drawing.Size(151, 21);
+            this.cmbMaterial.TabIndex = 39;
+            // 
+            // cmbTalla
+            // 
+            this.cmbTalla.FormattingEnabled = true;
+            this.cmbTalla.Location = new System.Drawing.Point(204, 108);
+            this.cmbTalla.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbTalla.Name = "cmbTalla";
+            this.cmbTalla.Size = new System.Drawing.Size(151, 21);
+            this.cmbTalla.TabIndex = 38;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(204, 245);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(151, 20);
+            this.dateTimePicker1.TabIndex = 37;
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(446, 70);
+            this.dgvProductos.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.RowHeadersWidth = 51;
+            this.dgvProductos.RowTemplate.Height = 24;
+            this.dgvProductos.Size = new System.Drawing.Size(343, 366);
+            this.dgvProductos.TabIndex = 36;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(39, 245);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 17);
+            this.label8.TabIndex = 35;
+            this.label8.Text = "Fecha Registro:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(39, 213);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 17);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "Cantidad:";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(204, 211);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(151, 20);
+            this.txtCantidad.TabIndex = 33;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(39, 178);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 17);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Marca";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 140);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 17);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Material:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 108);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Talla:";
+            // 
+            // txtCodProducto
+            // 
+            this.txtCodProducto.Location = new System.Drawing.Point(204, 70);
+            this.txtCodProducto.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCodProducto.Name = "txtCodProducto";
+            this.txtCodProducto.Size = new System.Drawing.Size(151, 20);
+            this.txtCodProducto.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Código Producto:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(327, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 31);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Producto";
+            // 
+            // PRODUCTO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInhabilitar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnNuevoMarca);
+            this.Controls.Add(this.btnNuevoMaterial);
+            this.Controls.Add(this.btnNuevoTalla);
+            this.Controls.Add(this.cmbMarca);
+            this.Controls.Add(this.cmbMaterial);
+            this.Controls.Add(this.cmbTalla);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCodProducto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "PRODUCTO";
+            this.Text = "PRODUCTO";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInhabilitar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnNuevoMarca;
+        private System.Windows.Forms.Button btnNuevoMaterial;
+        private System.Windows.Forms.Button btnNuevoTalla;
+        private System.Windows.Forms.ComboBox cmbMarca;
+        private System.Windows.Forms.ComboBox cmbMaterial;
+        private System.Windows.Forms.ComboBox cmbTalla;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodProducto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
